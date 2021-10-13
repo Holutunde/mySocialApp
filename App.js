8589720788f9f96a7e1b4742a2be2ea08a1e1fec
Holutunde <Holutunde99@gmail.com>
2021-10-13 17:36:50 +0100

    feature(Authentication): Onbarding Screen

--- App.js
+++ App.js
@@ -1,15 +1,45 @@
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import Onboarding from './screen/Onboarding'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your appiii!</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
+const Stack = createNativeStackNavigator()
+
+const App = () => {
+  const [launch, setLaunch] = useState(null)
+
+  useEffect(() => {
+    AsyncStorage.getItem('launched').then((value) => {
+      if (value == null) {
+        AsyncStorage.setItem('launched', 'true')
+        setLaunch(true)
+      } else {
+        setLaunch(false)
+      }
+    })
+  }, [])
+
+  if (launch === null) {
+    return null
+  } else if (launch === true) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Onboarding" component={Onboarding} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  }
+
+  return <Login />
 }
+export default App
 
 const styles = StyleSheet.create({
   container: {
