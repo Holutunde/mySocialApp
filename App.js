e0ae2fa84c8e6f62d65809a02a78d77fc1da9a19
Holutunde <Holutunde99@gmail.com>
2021-10-14 11:48:16 +0100

    feature(Prebuild): Added Andriod and IOS build

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import Providers from './navigation'
+import { useFonts } from 'expo-font'
 
 const App = () => {
+  let [fontLoaded] = useFonts({
+    'Lato-Bold': require('./assets/fonts/Lato-Bold.ttf'),
+    'Lato-Bolditalic': require('./assets/fonts/Lato-BoldItalic.ttf'),
+    'Lato-Italic': require('./assets/fonts/Lato-Italic.ttf'),
+    'Lato-Regular': require('./assets/fonts/Lato-Regular.ttf'),
+  })
+
+  if (!fontLoaded) {
+    return null
+  }
+
   return <Providers />
 }
 
